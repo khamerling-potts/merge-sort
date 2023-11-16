e54acef4c755566bef83adac6080e1b18119bc75
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2023-11-16 14:17:17 -0500

    done

--- javascript/merge_sort.js
+++ javascript/merge_sort.js
@@ -1,9 +1,32 @@
 function merge(arr1, arr2) {
   // type your code here
+  const result = [];
+  while (arr1.length > 0 && arr2.length > 0) {
+    const element1 = arr1[0];
+    const element2 = arr2[0];
+    if (element1 < element2) {
+      result.push(arr1.shift());
+    } else {
+      result.push(arr2.shift());
+    }
+  }
+  const leftover = arr1.length > arr2.length ? arr1 : arr2;
+  return result.concat(leftover);
 }
 
+//console.log(merge([3, 5, 9, 12], [4, 6, 13, 15]));
+
 function mergeSort(arr) {
+  debugger;
   // type your code here
+  if (arr.length <= 1) {
+    return arr;
+  }
+  const mid = Math.floor(arr.length / 2);
+  const left = mergeSort(arr.slice(0, mid));
+  const right = mergeSort(arr.slice(mid));
+
+  return merge(left, right);
 }
 
 if (require.main === module) {
